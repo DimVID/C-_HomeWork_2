c0282f16bd73ae4f40733bf410da0cedab10add5
DimVID <dimvid8612@ya.ru>
2023-02-20 20:12:53 +0300

    выполнил домашнее задание #2

---
+++ H_W_2.2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int userNumber = Prompt("\nВведите 3-х значное число и больше");
+if (Validation(userNumber)) System.Console.WriteLine($"\nТретья цифра числа {userNumber} > {ShowThreeDigitalNumber(userNumber)} ");
+else System.Console.WriteLine("\nТретьей цифры нет "); 
+
+int Prompt(string msg)
+{
+    System.Console.WriteLine($"{msg} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Validation(int num)
+{
+    if (num > 99) return true;
+    return false;
+}
+int ShowThreeDigitalNumber(int number)
+{
+    while (number >999)
+    {
+        number = number / 10;
+    }
+    return number % 10;
+}
---
+++ H_W_2.3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("ведите день недели");
+int weekday = Convert.ToInt32 (Console.ReadLine ());
+if (weekday == 1)
+{
+ System.Console.WriteLine("нет"); 
+}
+if (weekday == 2)
+{
+ System.Console.WriteLine("нет"); 
+}
+if (weekday == 3)
+{
+ System.Console.WriteLine("нет"); 
+}
+if (weekday == 4)
+{
+ System.Console.WriteLine("нет"); 
+}
+if (weekday == 5)
+{
+ System.Console.WriteLine("нет"); 
+}
+if (weekday == 6)
+{
+ System.Console.WriteLine("да");
+}
+if (weekday == 7)
+{
+ System.Console.WriteLine("да"); 
+}
+if (weekday < 1 || weekday > 7 )
+{
+    System.Console.WriteLine("такого дня нет");
+}
